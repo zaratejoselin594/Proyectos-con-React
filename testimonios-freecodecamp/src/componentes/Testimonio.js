27349c4057c081025926efbe2534b21c98dee037
Joselin Zarate <zaratejoselin594@gmail.com>
2023-08-05 20:37:58 -0300

    react

--- testimonios-freecodecamp/src/componentes/Testimonio.js
+++ testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -1,10 +1,9 @@
-import React from 'react';
 import "../css/Testimonio.css"
 
 function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
-      <img className="img-testimonio" src={require(`../img/${props.imagen}.png`)} alt="Img Shawn" />
+      <img className="img-testimonio" src={require(`../img/${props.imagen}.png`)} />
       <div className="contenedor-txt-testimonio">
         <p className="nombre-testimonio">
           <b>{props.nombre}</b> en {props.pais}
