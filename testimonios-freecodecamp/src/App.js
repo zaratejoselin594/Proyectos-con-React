45f7166e63d73a10027874b2aa51fb52d46e3b39
Joselin Zarate <zaratejoselin594@gmail.com>
2023-08-05 20:28:41 -0300

    react

--- testimonios-freecodecamp/src/App.js
+++ testimonios-freecodecamp/src/App.js
@@ -4,7 +4,12 @@ import Testimonio from "./componentes/Testimonio"
 function App() {
   return (
     <div className="App">
-      <Testimonio />
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimonio nombre="Shawn Wang" pais="Singapur" imagen="uno" profecion="Ingeniero de software" empreza="Amazon" textoTestimonio="Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando en los cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida." />
+        <Testimonio nombre="Sara Chima" pais="Nigeria" imagen="dos" profecion="Ingeniero de software" empreza=" ChatDesk" textoTestimonio="freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mi conocimiento de codificación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble." />
+        <Testimonio nombre="Emma Bostian" pais="Suecia" imagen="tres" profecion="Ingeniero de software" empreza="Spotify" textoTestimonio="Siempre he tenido problemas para aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify." />
+      </div>
     </div>
   );
 }
--- testimonios-freecodecamp/src/componentes/Testimonio.js
+++ testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import "../css/Testimonio.css"
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
-      <img className="img-testimonio" src={require("../img/uno.png")} alt="Img Shawn" />
+      <img className="img-testimonio" src={require(`../img/${props.imagen}.png`)} alt="Img Shawn" />
       <div className="contenedor-txt-testimonio">
-        <p className="nombre-testimonio">Shawn Wang en Singapur</p>
-        <p className="profecion-testimonio">Ingeniero de software en Amazon</p>
-        <p className="texto-testimonio">"Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando en los cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida " .</p>
+        <p className="nombre-testimonio">
+          <b>{props.nombre}</b> en {props.pais}
+        </p>
+        <p className="profecion-testimonio">
+          {props.profecion} en <b>{props.empreza}</b>
+        </p>
+        <p className="texto-testimonio">"{props.textoTestimonio}"</p>
       </div>
     </div>
   );
