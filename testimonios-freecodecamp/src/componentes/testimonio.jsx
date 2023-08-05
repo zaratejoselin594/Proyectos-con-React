20d0c4576036af603249046b7e5a367de46d3e56
Joselin Zarate <zaratejoselin594@gmail.com>
2023-08-05 11:58:56 -0300

    react

---
+++ testimonios-freecodecamp/src/componentes/testimonio.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function testimonio() {
+  return (
+    <div className="contenedor-testimonio">
+      <img className="img-testimonio" src="../img/uno.png" alt="" />
+    </div>
+  );
+}
