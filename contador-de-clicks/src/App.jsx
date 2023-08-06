94d64d117ed1d1f1034935c1c866aa01b97aee4b
Joselin Zarate <zaratejoselin594@gmail.com>
2023-08-06 12:09:52 -0300

    react

---
+++ contador-de-clicks/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import freeCodeCamp from './img/freecodecamp-logo.png'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='logo-contenedor'>
+        <img src={freeCodeCamp} alt="Logo de freecodecamp" className="freecodecamp-logo" />
+      </div>
+      <div className="contenedor-principal">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
