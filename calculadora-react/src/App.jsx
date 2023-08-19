40b5c6a2ce494d14bcd92add5e56f8d08c1033f1
Joselin Zarate <zaratejoselin594@gmail.com>
2023-08-19 10:11:05 -0300

    react

--- calculadora-react/src/App.jsx
+++ calculadora-react/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css';
+import Boton from './componentes/Boton'
 
 function App() {
   return (
     <div className="App">
       <div className="contenedor-calculadora">
-        
+        <div className="fila">
+          <Boton>1</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className="fila"></div>
+        <div className="fila"></div>
+        <div className="fila"></div>
+        <div className="fila"></div>
       </div>
     </div>
   );
---
+++ calculadora-react/src/componentes/Boton.jsx
@@ -0,0 +1,12 @@
+
+function Boton(props) {
+  const esOperador = valor => {
+    return isNaN(valor) && (valor != '.') && (valor != '=');
+  }
+  return (
+    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()} >
+      {props.children}
+    </div>
+  );
+}
+export default Boton; 
