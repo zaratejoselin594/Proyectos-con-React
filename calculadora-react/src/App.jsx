ca3cd6e771b47b5006ba36e98bc82f47bcdb4eb1
Joselin Zarate <zaratejoselin594@gmail.com>
2023-08-20 19:30:59 -0300

    reac

--- calculadora-react/src/App.jsx
+++ calculadora-react/src/App.jsx
@@ -28,28 +28,30 @@ function App() {
           <Boton manejarClick={agregarInput} >1</Boton>
           <Boton manejarClick={agregarInput} >2</Boton>
           <Boton manejarClick={agregarInput} >3</Boton>
-          <Boton manejarClick={agregarInput} >+</Boton>
+          <Boton manejarClick={agregarInput} >←</Boton>
         </div>
         <div className="fila">
           <Boton manejarClick={agregarInput} >4</Boton>
           <Boton manejarClick={agregarInput} >5</Boton>
           <Boton manejarClick={agregarInput} >6</Boton>
-          <Boton manejarClick={agregarInput} >-</Boton>
+          <Boton manejarClick={agregarInput} >+</Boton>
         </div>
         <div className="fila">
           <Boton manejarClick={agregarInput} >7</Boton>
           <Boton manejarClick={agregarInput} >8</Boton>
           <Boton manejarClick={agregarInput} >9</Boton>
-          <Boton manejarClick={agregarInput} >*</Boton>
+          <Boton manejarClick={agregarInput} >-</Boton>
         </div>
         <div className="fila">
-          <Boton manejarClick={calcularRes} >=</Boton>
           <Boton manejarClick={agregarInput} >0</Boton>
           <Boton manejarClick={agregarInput} >.</Boton>
           <Boton manejarClick={agregarInput} >/</Boton>
+          <Boton manejarClick={agregarInput} >*</Boton>
+
         </div>
         <div className="fila">
           <BtnClear manejarClear={()=> setInput('')} >Clear</BtnClear>
+          <Boton manejarClick={calcularRes} >=</Boton>
         </div>
       </div>
     </div>
