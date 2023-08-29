0d7b0400389fb4f3fcb334501374196b378ccd5c
Joselin Zarate <zaratejoselin594@gmail.com>
2023-08-29 20:30:01 -0300

    react

---
+++ aplicacion-tareas/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app-tareas">
+        <div className="tareas-listas-principal">
+          <h1>Mis Tareas</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
