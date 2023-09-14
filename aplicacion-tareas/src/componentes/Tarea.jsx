0d8225f695a51dbd9b0900cf85999778924c373e
Joselin Zarate <zaratejoselin594@gmail.com>
2023-09-14 11:54:43 -0300

    react

--- aplicacion-tareas/src/componentes/Tarea.jsx
+++ aplicacion-tareas/src/componentes/Tarea.jsx
@@ -5,7 +5,7 @@ import { IoPencilOutline } from "react-icons/io5";
 function Tarea({ id, texto, completada, completarTarea, eliminarTarea }) {
   return (
     <div className={completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
-      <div className="tarea-texto" onClick={() => completarTarea(id)}>
+      <div className="tarea-texto" onClick={() => completarTarea(id)}> 
         {texto}
       </div>
         <div  className='iconos'>
