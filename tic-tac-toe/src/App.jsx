724b7d3fcbd6955ecee9afc767510d9e5dda280f
Joselin Zarate <zaratejoselin594@gmail.com>
2023-09-19 11:24:45 -0300

    react

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import './App.css';
+
+
+const Turns = {
+  X: 'x',
+  O: 'o'
+}
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(Turns.X)
+  const updateBoard = (index) => {
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    const newTurn = turn === Turns.X ? Turns.O : Turns.X;
+    setTurn(newTurn)
+  }
+
+  return (
+    <main className='board'>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </ Square>
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === Turns.X}>
+          {Turns.X}
+        </Square>
+        <Square isSelected={turn === Turns.O}>
+          {Turns.O}
+        </Square>
+      </section>
+    </main>
+  );
+}
+
+export default App;
